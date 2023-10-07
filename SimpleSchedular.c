99857846166bc7cc833f6a3a55806858332e58bc
shrutya22487 <shrutya22487@iiitd.ac.in>
2023-10-07 10:32:25 +0530

    new files

---
+++ SimpleSchedular.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include <stdbool.h>
+#include <signal.h> 
+#include <sys/time.h>
+#include <time.h>
---
+++ simple-shell.h
@@ -0,0 +1,4 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#endif
