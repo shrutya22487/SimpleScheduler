81a2929b25b74a9bec74c610f797a0b85516cf30
shrutya22487 <shrutya22487@iiitd.ac.in>
2026-04-17 12:34:22 +0530

    moved sample run files to separate folder

---
+++ sample_programs/fib.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include<stdio.h>
+
+int fib(int n) {
+  if(n<2) return n;
+  else return fib(n-1)+fib(n-2);
+}
+
+
+int main(int argc, char const *argv[])
+{
+  int val = fib(40);
+  printf("fib value  ==  %d \n", val);
+  return 0;
+}
